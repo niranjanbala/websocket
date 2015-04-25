c20267623870a9f2e61edaf6e5326524f740062f
Niranjan_Bala <niranjan.veerasekaran@commonfloor.com>
2015-04-25 19:38:25 +0530

    test

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.io.route('drawClick', function(req) {
 })
 
 // Send client html.
-app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/public/client.html')
-})
+//app.get('/', function(req, res) {
+    //res.sendfile(__dirname + '/public/client.html')
+//})
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'));
